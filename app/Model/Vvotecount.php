30e1bb8da0c17875a8f6b5627209a688746bdc66
VincentCharpentier <animous.man.vc@gmail.com>
2013-06-18 21:16:43 +0200

    modif pour vote

--- app/Model/Vvotecount.php
+++ app/Model/Vvotecount.php
@@ -7,42 +7,4 @@ App::uses('AppModel', 'Model');
  */
 class Vvotecount extends AppModel {
 
-	//public $useTable = false; // This model does use a database table
-	/*
-	public $validate = array(
-			'message' => array(
-					'message' => array(
-							'rule' => array('minlength', 50),
-							//'allowEmpty' => false,
-							'message'    => 'Votre message doit contenir au moins 50 caractères.'
-					)
-			),
-			'email' => array(
-					'email' => array(
-							'rule' => array('maxLength', 100),
-							'last'    => false, // Permet d'afficher à la fois le message d'erreur associé à rule1 et à rule2 si rule1 et rule2 échouent
-							'message'    => 'L\'adresse email ne doit pas dépasser 100 caractères.'
-					),
-					'email' => array(
-							'rule'    => array('email', false),
-							'message' => 'L\adresse email n\'est pas valide.'
-					)
-			),
-			'nom' => array(
-					'nom' => array(
-							'rule' => array('maxLength', 50),
-							'last'    => false, // Permet d'afficher à la fois le message d'erreur associé à rule1 et à rule2 si rule1 et rule2 échouent
-							'message'    => 'Le nom ne doit pas dépasser 50 caractères.'
-					)
-			),
-			'prenom' => array(
-					'prenom' => array(
-							'rule' => array('maxLength', 50),
-							'last'    => false, // Permet d'afficher à la fois le message d'erreur associé à rule1 et à rule2 si rule1 et rule2 échouent
-							'message'    => 'Le prénom ne doit pas dépasser 50 caractères.'
-					)
-			)
-	);
-	*/
-
 }
